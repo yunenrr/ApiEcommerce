be76e14f738439924edee8cc58246c89cceea2b3
Yunen Ramos <yunencr@outlook.com>
2025-08-07 21:09:34 -0600

    Crear clase CategoryRepository

---
+++ ApiEcommerce/Repository/CategoryRepository.cs
@@ -0,0 +1,59 @@
+using System;
+using ApiEcommerce.Repository.IRepository;
+
+namespace ApiEcommerce.Repository;
+
+public class CategoryRepository : ICategoryRepository
+{
+    private readonly ApplicationDbContext _db;
+
+    public CategoryRepository(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+
+    public bool CategoryExists(int id)
+    {
+        return _db.Categories.Any(c => c.Id == id);
+    }
+
+    public bool CategoryExists(string name)
+    {
+        return _db.Categories.Any(c => c.Name.ToLower().Trim() == name.ToLower().Trim());
+    }
+
+    public bool CreateCategory(Category category)
+    {
+        category.CreationDate = DateTime.Now;
+        _db.Categories.Add(category);
+        return Save();
+    }
+
+    public bool DeleteCategory(Category category)
+    {
+        _db.Categories.Remove(category);
+        return Save();
+    }
+
+    public ICollection<Category> GetAllCategories()
+    {
+        return _db.Categories.OrderBy(c => c.Name).ToList();
+    }
+
+    public Category GetCategory(int id)
+    {
+        return _db.Categories.FirstOrDefault(c => c.Id == id) ?? throw new InvalidOperationException($"La categoría con el id {id} no existe.");
+    }
+
+    public bool Save()
+    {
+        return _db.SaveChanges() >= 0;  // Devuelve true si se guardaron cambios
+    }
+
+    public bool UpdateCategory(Category category)
+    {
+        category.CreationDate = DateTime.Now;
+        _db.Categories.Update(category);
+        return Save();
+    }
+}
