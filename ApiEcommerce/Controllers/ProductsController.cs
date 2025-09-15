66829ec83dc9a7cedb8f6033e3471c730f570d98
Yunen Ramos <yunencr@outlook.com>
2025-09-14 18:13:40 -0600

    Crear método GetProduct

--- ApiEcommerce/Controllers/ProductsController.cs
+++ ApiEcommerce/Controllers/ProductsController.cs
@@ -22,11 +22,30 @@ namespace ApiEcommerce.Controllers
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status403Forbidden)] // El usuario no está autorizado para ingresar a este recurso
         [ProducesResponseType(typeof(List<ProductDto>), StatusCodes.Status200OK)]
-        public IActionResult GetCategories()
+        public IActionResult GetProducts()
         {
             var products = _productRepository.GetProducts();
             var productsDto = _mapper.Map<List<ProductDto>>(products);
             return Ok(productsDto);
+        }
+
+        [HttpGet("{productId:int}", Name = "GetProduct")] // Nombre de la ruta
+        [ProducesResponseType(StatusCodes.Status403Forbidden)] // El usuario no está autorizado para ingresar a este recurso
+        [ProducesResponseType(StatusCodes.Status400BadRequest)] // El usuario envió una petición incorrecta
+        [ProducesResponseType(StatusCodes.Status404NotFound)] // No se encontró el recurso
+        [ProducesResponseType(typeof(ProductDto), StatusCodes.Status200OK)]
+        public IActionResult GetProduct(int productId)
+        {
+            var product = _productRepository.GetProduct(productId);
+
+            if (product == null)
+            {
+                return NotFound($"El producto con el id {productId} no existe.");
+            }
+
+            var productDto = _mapper.Map<ProductDto>(product);
+
+            return Ok(productDto);
         }
     }
 }
