e82068149775b1c0f2fb65dad3a02543fc489a50
Yunen Ramos <yunencr@outlook.com>
2025-10-07 19:23:45 -0600

    Implementar método Login

--- ApiEcommerce/Repository/UserRepository.cs
+++ ApiEcommerce/Repository/UserRepository.cs
@@ -1,17 +1,24 @@
 using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using ApiEcommerce.Models;
 using ApiEcommerce.Models.Dtos;
 using ApiEcommerce.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 namespace ApiEcommerce.Repository;
 
 public class UserRepository : IUserRepository
 {
     private readonly ApplicationDbContext _db;
+    private string? secretKey;
 
-    public UserRepository(ApplicationDbContext db)
+    public UserRepository(ApplicationDbContext db, IConfiguration configuration)
     {
         _db = db;
+        secretKey = configuration.GetValue<string>("ApiSettings:SecretKey");
     }
 
     public User? GetUser(int id)
@@ -29,9 +36,75 @@ public class UserRepository : IUserRepository
         return !_db.Users.Any(u => u.Username.ToLower().Trim() == username.ToLower().Trim());
     }
 
-    public Task<UserLoginResponseDto?> Login(UserLoginDto userLoginDto)
+    public async Task<UserLoginResponseDto?> Login(UserLoginDto userLoginDto)
     {
-        throw new NotImplementedException();
+        if (string.IsNullOrEmpty(userLoginDto.Username))
+        {
+            return new UserLoginResponseDto
+            {
+                Token = "",
+                User = null,
+                Message = "El Username es requerido"
+            };
+        }
+
+        var user = await _db.Users.FirstOrDefaultAsync<User>(u => u.Username.ToLower().Trim() == userLoginDto.Username.ToLower().Trim());
+
+        if (user == null)
+        {
+            return new UserLoginResponseDto
+            {
+                Token = "",
+                User = null,
+                Message = "Username no encontrado"
+            };
+        }
+
+        if (!BCrypt.Net.BCrypt.Verify(userLoginDto.Password, user.Password)) // Verificar la contraseña encriptada
+        {
+            return new UserLoginResponseDto
+            {
+                Token = "",
+                User = null,
+                Message = "Credenciales son incorrectas"
+            };
+        }
+
+        // JWT Generación
+        var handlerToken = new JwtSecurityTokenHandler();
+
+        if (string.IsNullOrEmpty(secretKey))
+        {
+            throw new InvalidOperationException("SecretKey no está configurada.");
+        }
+
+        var key = Encoding.UTF8.GetBytes(secretKey);
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new[] // Arreglo de Claims
+            {
+                new Claim("id", user.Id.ToString()),
+                new Claim("username", user.Id.ToString()),
+                new Claim(ClaimTypes.Role, user.Role ?? string.Empty) // Usar cadena vacía si Role es null
+            }),
+            Expires = DateTime.UtcNow.AddHours(2),
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature) // Algoritmo de encriptación
+        };
+
+        var token = handlerToken.CreateToken(tokenDescriptor);
+
+        return new UserLoginResponseDto
+        {
+            Token = handlerToken.WriteToken(token),
+            User = new UserRegisterDto
+            {
+                Name = user.Name,
+                Username = user.Username,
+                Role = user.Role,
+                Password = user.Password ?? string.Empty
+            },
+            Message = "Usuario logueado correctamente"
+        };
     }
 
     public async Task<User?> Register(CreateUserDto createUserDto)
@@ -42,7 +115,7 @@ public class UserRepository : IUserRepository
             Name = createUserDto.Name,
             Username = createUserDto.Username ?? "No Username",
             Password = encryptedPassword,
-            Role = "admin"
+            Role = createUserDto.Role
         };
         _db.Users.Add(user);
         await _db.SaveChangesAsync(); // Guardar cambios de forma asíncrona
