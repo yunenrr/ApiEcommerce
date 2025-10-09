50ea682bd3bfa53bf5df0bad516a1145647d1b76
Yunen Ramos <yunencr@outlook.com>
2025-10-08 19:05:14 -0600

    Crear método RegisterUser

--- ApiEcommerce/Controllers/UsersController.cs
+++ ApiEcommerce/Controllers/UsersController.cs
@@ -27,5 +27,56 @@ namespace ApiEcommerce.Controllers
             var usersDto = _mapper.Map<List<UserDto>>(users);
             return Ok(usersDto);
         }
+
+        [HttpGet("{id:int}", Name = "GetUser")]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ProductDto), StatusCodes.Status200OK)]
+        public IActionResult GetUser(int id)
+        {
+            var user = _userRepository.GetUser(id);
+
+            if (user == null)
+            {
+                return NotFound($"El usuario con el id {id} no existe.");
+            }
+
+            var userDto = _mapper.Map<UserDto>(user);
+            return Ok(userDto);
+        }
+
+        [HttpPost(Name = "RegisterUser")]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> RegisterUser([FromBody] CreateUserDto createUserDto)
+        {
+            // ¿Qué es ModelState? Es una propiedad del controlador que contiene el estado de la validación del modelo.'
+            if (createUserDto == null || !ModelState.IsValid) // Validación de que el objeto no sea nulo y que el modelo sea válido.
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (string.IsNullOrWhiteSpace(createUserDto.Username))
+            {
+                return BadRequest("Username es requerido.");
+            }
+
+            if (!_userRepository.IsUniqueUser(createUserDto.Username))
+            {
+                return BadRequest("El usuario ya existe.");
+            }
+
+            var result = await _userRepository.Register(createUserDto);
+
+            if (result == null)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error al registrar al usuario.");
+            }
+            
+            return CreatedAtRoute("GetUser", new { id = result.Id }, result);
+        }
     }
 }
